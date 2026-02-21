36dacee2409e39a2d544264098569845fbca9863
Buseong Kim <flight@skyline23.com>
2026-02-21 18:30:44 +0900

    cask: add applepie-rpc 0.1.17

---
+++ Casks/applepie-rpc.rb
@@ -0,0 +1,22 @@
+cask "applepie-rpc" do
+  version "0.1.17"
+  sha256 "e8701ca03b62fb5b2faebf0bfa3bae7cc284d268ffb39f723e47f85246c2a972"
+
+  url "https://github.com/Skyline-23/Applepie-RPC/releases/download/v#{version}/Applepie-RPC-v#{version}.dmg",
+      verified: "github.com/Skyline-23/Applepie-RPC/"
+  name "Applepie-RPC"
+  desc "macOS menu bar app that publishes now-playing info to Discord Rich Presence"
+  homepage "https://github.com/Skyline-23/Applepie-RPC"
+
+  livecheck do
+    url "https://github.com/Skyline-23/Applepie-RPC/releases/latest/download/appcast.xml"
+    strategy :sparkle
+  end
+
+  app "Applepie-RPC.app"
+
+  zap trash: [
+    "~/Library/Application Support/Applepie-RPC",
+    "~/Library/Preferences/com.skyline23.Applepie-RPC.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# homebrew-applepie-rpc
+
+Homebrew tap for Applepie-RPC.
+
+## Install
+
+```bash
+brew tap skyline-23/applepie-rpc
+brew install --cask applepie-rpc
+```
+
+## Upgrade
+
+```bash
+brew upgrade --cask applepie-rpc
+```
